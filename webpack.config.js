51c736ee86ad389a5f770d6c217504ded90b762c
Rizwan Choudhury <choudhuryrizwan97@gmail.com>
2020-07-03 17:00:50 -0700

    add multiple entries in webpack.config to run webpack from proxy

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ var DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
   entry: {
-    'Nav_Bar': path.join(__dirname, 'Nav_Bar', 'client', 'src', 'index.jsx')
+    'Nav_Bar': path.join(__dirname, 'Nav_Bar', 'client', 'src', 'index.jsx'),
+    'Product_Info': path.join(__dirname, 'Product_Info', 'client', 'src', 'index.jsx'),
+    'Full_Look': path.join(__dirname, 'Full_Look', 'client', 'src', 'index.jsx'),
+    'Reviews': path.join(__dirname, 'Reviews', 'client', 'src', 'index.jsx')
   },
   output: {
     filename: '[name].bundle.js',
@@ -17,7 +20,13 @@ module.exports = {
         loader: 'babel-loader',
         query: {
           presets: ['react', 'es2015']
-        }
+        },
+        include: [
+          path.join(__dirname, 'Nav_Bar', 'client', 'src'),
+          path.join(__dirname, 'Product_Info', 'client', 'src'),
+          path.join(__dirname, 'Full_Look', 'client', 'src'),
+          path.join(__dirname, 'Reviews', 'client', 'src')
+        ]
       },
       {
         test: /\.svg$/,
